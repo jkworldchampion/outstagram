289e31ecb19be2ba5eccfe1ee83309dcf4a50b30
juhwan <hi@hiui-MacBookAir.local>
2023-05-16 18:28:45 +0900

    finish BottomTabs.js

--- components/home/BottomTabs.js
+++ components/home/BottomTabs.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View, StyleSheet, Image } from 'react-native'
+import { Divider } from 'react-native-elements'
 
 
 export const bottomTabIcons = [
@@ -37,14 +38,56 @@ const BottomTabs = ({icons}) => {
 
   const Icon = ({icon}) => (
     <TouchableOpacity onPress={() => setActiveTab(icon.name)}>
-      <Image />
+      <Image 
+        source={{ uri: activeTab == icon.name ? icon.active : icon.inactive }} 
+        style={[styles.icon,
+          icon.name == 'Profile' ? styles.profilePic() : null,
+          activeTab == 'Profile' && icon.name == activeTab 
+          ? styles.profilePic(activeTab)
+          : null,
+        ]}
+      />
     </TouchableOpacity>
   )
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.wrapper}>
+      <Divider width={1} orientation='vertical'/>
+      <View style={styles.container}>
+        {icons.map((icon, index) => (
+          <Icon key={index} icon={icon} />
+        ))}
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    position: 'absolute',
+    width: '100%',
+    bottom: '3%',
+    zIndex: 999,
+    backgroundColor: '#FFF'
+  },
+
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 50,
+    paddingTop: 10,
+  },
+
+  icon: {
+    width: 30,
+    height: 30,
+
+  },
+
+  profilePic: (activeTab = '') => ({
+    borderRadius: 50,
+    borderWidth: activeTab == 'Profile' ? 2 : 0,
+    backgroundColor: '#FFF'
+  })
+})
 
 export default BottomTabs
