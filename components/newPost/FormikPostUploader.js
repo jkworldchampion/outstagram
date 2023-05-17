f5bdbebd9106e513f28b655183f2f2c34dc187fa
juhwan <jkworldchampion@gmail.com>
2023-05-17 14:58:41 +0900

    add image, textinput in FormikPostUploader.js

--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -2,8 +2,9 @@ import React, { Component, useState } from 'react'
 import { Text, TextInput, View, Image } from 'react-native'
 import * as Yup from 'yup'
 import { Formik } from 'formik'
+import { Divider } from 'react-native-elements'
 
-const PLACEHOLDER_IMG = ''
+const PLACEHOLDER_IMG = 'https://cdn2.vectorstock.com/i/1000x1000/17/61/select-image-vector-11591761.jpg'
 
 const uploadPostSchema = Yup.object().shape({
   imageUrl: Yup.string().url().required('A url is required'),
@@ -23,12 +24,35 @@ const FormikPostUploader = () => {
 
       {({ handleBlur, handleChange, handleSubmit, values, errors, isValid }) => (
         <>
-          <View>
-            <Image />
-          </View>
+          <View style={{ margin: 20, justifyContent: 'space-between', flexDirection: 'row' }}>
+            <Image source={{ uri: PLACEHOLDER_IMG }} style={{ width: 100, height: 100 }}/>
 
-          <TextInput placeholder='Write a caption ...'/>
-          <TextInput placeholder='Enter Image Url'/>
+            <View style={{ flex: 1, marginLeft: 12 }}>
+              <TextInput 
+                style={{ color: 'black', fontSize: 20 }}
+                placeholder='Write a caption ...'
+                placeholderTextColor='gray'
+                multiline={true}
+                onChangeText={handleChange('caption')}
+                onBlur={handleBlur('caption')}
+                // value={values.caption}
+              />
+            </View>
+          </View>
+          <Divider width={0.2} orientation='vertical'/>
+          <TextInput 
+            style={{ color: 'black' , fontSize: 18 }}
+            placeholder='Enter Image Url'
+            placeholderTextColor='gray'
+            onChangeText={handleChange('imageUrl')}
+            onBlur={handleBlur('imageUrl')}
+            // value={values.imageUrl}
+          />
+          {errors.imageUrl && (
+            <Text style={{ fontSize: 10, color: 'red' }}>
+              {errors.imageUrl}
+            </Text>
+          )}
         </>
       )}
 
